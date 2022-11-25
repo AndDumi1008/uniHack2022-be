682f6ca88cf035fa92b82ce7de237e5c78a288a1
Andrei Dumitrescu <andrei.dumitrescu100801@gmail.com>
2022-11-25 18:12:40 +0200

    initialize project

---
+++ src/main/java/com/unihack/unihackbe/UniHackBeApplication.java
@@ -0,0 +1,13 @@
+package com.unihack.unihackbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniHackBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniHackBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unihack/unihackbe/UniHackBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unihack.unihackbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniHackBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
